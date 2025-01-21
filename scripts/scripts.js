a3c9e3d7a9b6279e0ee77b98b95d385a0a0edbe6
gdudek <gdudek.dev@gmail.com>
2025-01-21 23:11:46 +0100

    feat/Ajout des sections hero et about (pas de responsive pour l'instant

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,18 @@
-const btn = document.querySelector(".menu-hamburger");
-const img=document.getElementById("btn-hamburger")
-btn.addEventListener("click", () => {
-    if(img.classList==="icon-hamburger"){
-    img.classList.remove("icon-hamburger");
-    img.classList.add("icon-close");
-    }else{
-        img.classList.remove("icon-close");
-        img.classList.add("icon-hamburger");
-    }
+// Gestion du click bouton et du menu qui apparait
 
+const imgHamburger = document.getElementById("btn-hamburger-hamburger");
+const menu= document.querySelector(".affichage-hamburger") 
+const imgClose = document.getElementById("btn-hamburger-close");
+
+imgHamburger.addEventListener("click", () => {
+    menu.style.right=0;
 });
+imgClose.addEventListener("click", () =>{
+    menu.style.right='-100%';
+    
+})
+window.addEventListener("resize", () => {
+    if(window.innerWidth>='768px'){
+        menu.style.right='-100%';
+    }
+})
